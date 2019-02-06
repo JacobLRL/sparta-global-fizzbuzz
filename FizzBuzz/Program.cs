2be7a54b6eb42d14270fa77054a993740f1a30ea
JacobLRL <JLittle@spartaglobal.com>
2019-02-06 10:23:56 +0000

    Uploading fizzbuzz code

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++) {
+                Console.WriteLine(FizzBuzz.Fizz(i));
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class FizzBuzz {
+        public static string Fizz(int i) {
+            return (i % 15 == 0) ? "FizzBuzz" : ((i % 3 == 0) ? "Fizz" : ((i % 5 == 0) ? "Buzz" : i.ToString()));
+        }
+    }
+}
